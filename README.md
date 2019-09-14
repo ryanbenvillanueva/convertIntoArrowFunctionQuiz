dd3edca43cb9188d93ebdb0ebf6e73db37bc0d6e
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-09-14 13:48:24 +0800

    fix: Provide solution to the convertIntoArrowFunction quiz

---
+++ README.md
@@ -0,0 +1,4 @@
+# _Convert Function into an Arrow Functioin Quiz_
+#### _September 14, 2019_
+#### _This is my solution to the quiz from Udacity's free ES6 course._
+#### _By Ryan Ben S. Villanueva_
---
+++ convertIntoArrowFunction.js
@@ -0,0 +1,11 @@
+// Directions:
+// Convert the function passed to the map() method into an arrow function.
+
+/*
+ * Programming Quiz: Convert Function into an Arrow Function (2-1)
+ */
+
+// convert to an arrow function
+const squares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(square => square * square);
+
+console.log(...squares);
